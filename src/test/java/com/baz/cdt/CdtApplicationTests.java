1304f2d56520fff8a0ef6d4dc82bfdd93dd00006
180794 <rigobertom.martinez@gmail.com>
2021-11-17 23:57:43 -0600

    PrimerComit

---
+++ src/test/java/com/baz/cdt/CdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baz.cdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
